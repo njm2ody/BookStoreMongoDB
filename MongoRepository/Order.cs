c33b99701979eee2c7dcba9741d7194094904177
njm2ody <njm2ody@ro.ru>
2014-03-31 23:02:05 +0700

    add Date field

--- MongoRepository/Order.cs
+++ MongoRepository/Order.cs
@@ -13,30 +13,36 @@ namespace MongoRepository
         [BsonId]
         public ObjectId Id { get; set; }
 
-        public Client Client { get; set; }
+        private Client _Client;
+        public Client Client { get { return _Client; } set { _Client = value;} }
 
         private List<Book> _Bucket;
         public List<Book> Bucket
             {
-                set{}
+                set { _Bucket = value; }
 
                 get { return _Bucket; }
             }
 
-        public double TotalPrice {set; get;}
+       // public double TotalPrice {set; get;}
+        private DateTime _Date;
+        public DateTime Date { set { _Date = value; } get { return _Date; } }
 
-        public Order() { this.Bucket = new List<Book>(); }
-        public Order(Client client, List<Book> list_of_book) { this.Client = client; this.Bucket = list_of_book; }
+        public Order() { this.Bucket = new List<Book>(); this.Date = new DateTime(); }
 
-        //public void setBucket(List<Book> lst) 
-        //{
-        //    double sum = 0;
-        //    foreach (Book i in lst) 
-        //    {
-        //        sum += i.Price;
-        //    }
+        public Order(Client client, List<Book> list_of_book) 
+        { 
+          this.Client = client; 
+          this.Bucket = list_of_book;
+          this.Date = new DateTime();
+        }
 
-        //    TotalPrice = sum;
-        //}
+        public Order(Client client, List<Book> list_of_book, DateTime date)
+        {
+            this.Client = client;
+            this.Bucket = list_of_book;
+            this.Date =   date;
+        }
+       
     }
 }
